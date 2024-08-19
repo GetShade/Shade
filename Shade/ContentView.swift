325ad95e0ae0f1ec906c994aa1df951d961cd2cf
Omar Radwan <omar.radwan@yassir.com>
2024-08-19 10:53:46 +0300

    Init Commit

---
+++ Shade/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shade
+//
+//  Created by O M A R on 18/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shade/ShadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadeApp.swift
+//  Shade
+//
+//  Created by O M A R on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
